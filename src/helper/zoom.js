3f5220c0cf7b3cee4ee168381eaccea2fccdead8
Stefan Koch <stefan@bermuda27f.de>
2022-01-30 23:55:37 +0100

    20220130 7

--- src/helper/zoom.js
+++ src/helper/zoom.js
@@ -1,9 +1,7 @@
 import { getElement, setTimeOffset } from './calc_set.js'
 import * as d3_zoom from 'd3-zoom';
 
-export function zoomToElement (props, type, key, element){
-
-    const { state : state } = props;
+export function zoomToElement ({ state }, type, key, element){
 
     const calcEl = () => {
         switch(type){
