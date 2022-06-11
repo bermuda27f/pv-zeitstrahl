9cbb4f517f0cd3aff7a9cdd12775250b7b797132
Stefan Koch <stefan@bermuda27f.de>
2022-06-12 01:18:47 +0200

    20220612 1

--- src/helper/calc_set.js
+++ src/helper/calc_set.js
@@ -1,8 +1,6 @@
 import { calc_xScale, calc_yScale } from './scale.js';
 
 import * as d3_axis from 'd3-axis';
-import * as d3_time from 'd3-time';
-import * as d3_timeFormat from 'd3-time-format';
 import * as d3_format from 'd3-format'
 
 export function getPatterns(type){
@@ -20,39 +18,6 @@ export function getElement (y, key_type, key) {
       return x[key_type] === key;
   })
 }
-
-export function setTime (date){
-
-  const locale = d3_timeFormat.timeFormatLocale({
-    dateTime : "%A, %e %B %Y г. %X",
-    date : "%d.%m.%Y",
-    time : "%H:%M:%S",
-    periods : ["AM", "PM"],
-    days : ["Sonntag", "Montag", "Dienstag", "Mittwoch", "Donnerstag", "Freitag", "Samstag"],
-    shortDays : ["So.", "Mo.", "Di.", "Mi.", "Do.", "Fr.", "Sa."],
-    months : ["Januar", "Februar", "März", "April", "Mai", "Juni", "Juli", "August", "September", "Oktober", "November", "Dezember"],
-    shortMonths : ["Jan.", "Feb.", "Mrz.", "Apr.", "Mai", "Jun.", "Jul.", "Aug.", "Sept.", "Okt.", "Nov.", "Dez."]
-  });
-
-  const formatMillisecond = locale.format(".%L"),
-  formatSecond = locale.format(":%S"),
-  formatMinute = locale.format("%I:%M"),
-  formatHour = locale.format("%I %p"),
-  formatDay = locale.format("%a %d"),
-  formatWeek = locale.format("%b %d"),
-  formatMonth = locale.format("%B"),
-  formatYear = locale.format("%Y");
-
-  return (
-      d3_time.timeSecond(date) < date ? formatMillisecond
-    : d3_time.timeMinute(date) < date ? formatSecond
-    : d3_time.timeHour(date) < date ? formatMinute
-    : d3_time.timeDay(date) < date ? formatHour
-    : d3_time.timeMonth(date) < date ? (d3_time.timeWeek(date) < date ? formatDay : formatWeek)
-    : d3_time.timeYear(date) < date ? formatMonth
-    : formatYear)(date);
-
-} 
 
 export function setTimeOffset (state, element, type ){
 
