ec7a6b7c63bfd137e704c0b772cf554374e27607
Stefan Koch <stefan@bermuda27f.de>
2022-06-26 19:51:05 +0200

    20220626 4

--- src/index.js
+++ src/index.js
@@ -43,7 +43,7 @@ function App (){
 
         width : null,
         mainRef : mainRef,
-        maxScale : 70,
+        maxScale : 6,
         dateOptions : { year: 'numeric', month: 'long' },
         dateOptionsStory : { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' },
         transition : getTransition,
